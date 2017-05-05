61bcd3f5811d5da34dead17e8ed69b064bb30901
Yann Braga <yannbf@gmail.com>
2017-05-04 22:31:50 -0300

    feat(menu) Add selected menu highlight

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { Subject } from 'rxjs';
 
 @Component({
   templateUrl: 'app.html'
@@ -14,17 +15,24 @@ export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
   rootPage: any = HomePage;
+  activePage = new Subject();
 
-  pages: Array<{title: string, component: any}>;
+  pages: Array<{ title: string, component: any, active: boolean, icon: string }>;
 
   constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'Home', component: HomePage, active: true, icon: 'home' },
+      { title: 'List Page', component: ListPage, active: false, icon: 'alarm' },
     ];
+
+    this.activePage.subscribe((selectedPage: any) => {
+      this.pages.map(page => {
+        page.active = page.title === selectedPage.title;
+      });
+    });
 
   }
 
@@ -41,5 +49,6 @@ export class MyApp {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
     this.nav.setRoot(page.component);
+    this.activePage.next(page);
   }
 }
